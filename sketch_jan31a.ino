690ec42483f28c801c5791704470ee5fe91d3247
asatkeeva <32940086+asatkeeva@users.noreply.github.com>
2018-02-02 15:52:47 -0800

    Update sketch_jan31a.ino

--- sketch_jan31a.ino
+++ sketch_jan31a.ino
@@ -9,50 +9,65 @@ int BUTTON_yellow = 9;
 int BUTTON_blue = 8;
 int BUTTON_green = 13;
 int BUTTON_red = 10;
-int PTS = 0; // need to have 2 seperate couners for each player
-int turnCount = 0;
+int P1SET = 0;
+int P2SET = 0;
+int P1GAME = 0;
+int P2GAME = 0;
+int 
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Player1 Player2 ");
+  lcd.setCursor(7, 0);
+  lcd.print("-");
+  lcd.setCursor(7, 1);
+  lcd.print("-");
 }
 
-//Minus button are interior buttons, green and red
 void loop() {
+  // Increments score for player 1
     if(digitalRead(BUTTON_blue) == HIGH){
-      delay(150);
-      turnCount++;
-      PTS = PTS + 1;
-      if (turnCount / 2 % 2 == 0){
-       //turn off this LED, light up the other LED
-       //if one keep this LED on 
-      }
-      }else if (digitalRead(BUTTON_red) == HIGH){
-        delay(150);
-        PTS = PTS - 1;
-      }
-    if(digitalRead(BUTTON_yellow) == HIGH){
-      delay(150);
-      turnCount++;
-      PTS = PTS - 1;
-       if (turnCount / 2 % 2 == 0){
-       //turn off this LED, light up the other LED
-       //if one keep this LED on 
-      }
-
-    }else if (digitalRead(BUTTON_green) == HIGH){
-      delay(150);
-      PTS = PTS - 1;
+      P1SET++;
+      delay(500);
+    } //Decrements score for player 1
+    else if(digitalRead(BUTTON_red) == HIGH && P1SET > 0){
+      P1SET--;
+      delay(500);
     }
+  // Increments score for player 2
+    if(digitalRead(BUTTON_yellow) == HIGH){
+      P2SET++;
+      delay(500);
+    } //Decrements score for player 2
+    else if(digitalRead(BUTTON_green) == HIGH && P2SET > 0){
+      P2SET--;
+      delay(500);
+    }
+
+// Increments the gamescore of player 1
+    if(P1SET == 11){
+      P1SET = 0;
+      P2SET = 0;
+      P1GAME++;
+    }
+// Increments the gamescore of player 2
+    if(P2SET == 11){
+      P1SET = 0;
+      P2SET = 0;
+      P2GAME++;
+    }
+
+
     
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-//  lcd.print(millis() / 1000);
-  lcd.print(PTS);
-  
-//  delay(150);
+  lcd.setCursor(5, 0);
+  lcd.print(P1GAME);
+  lcd.setCursor(13, 0);
+  lcd.print(P2GAME);  
+  lcd.setCursor(5, 1);
+  lcd.print(P1SET);
+  lcd.setCursor(13, 1);
+  lcd.print(P2SET);
 }
